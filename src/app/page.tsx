e01b8e8505fe4bdc4564d19934597dde572bda41
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-12 19:15:49 +0500

    adding next auth authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import { useSession , signIn , signOut } from "next-auth/react";
 import RegisterPage from "./register/page";
 import LoginPage from "./login/page";
 
 export default function Home() {
+  const { data : session } = useSession;
   return (
     <main className="w-full h-auto flex justify-center items-center">
       {/* <RegisterPage /> */}
