057d17ed9d264891e606a765fa854705e13bdb0d
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-12 18:09:25 +0500

    created

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -16,13 +16,13 @@ const LoginPage = () => {
                     <Logo />
                 </div>
 
-                <div className='w-full h-4/5 flex flex-col justify-start gap-6 items-center'>
+                <div className='w-full h-4/5 flex flex-col justify-start gap-4 items-center'>
                     {/* heading */}
-                    <h1 className='text-5xl font-medium'>Welcome Back</h1>
+                    <h1 className='text-4xl font-medium'>Welcome Back</h1>
                     <p className='text-sm font-mono text-foreground/60'>It's time to check your sales</p>
 
                     {/* login with Google */}
-                    <div className='w-auto h-auto py-2'>
+                    <div className='w-auto h-auto py-3'>
                         <Button className='min-w-sm'> <Image src='/icon/google-icon.png' alt='Google Icon' width={20} height={20} />  Continue with Google</Button>
                     </div>
                     <div className=' flex flex-col gap-4 justify-center items-center'>
@@ -32,7 +32,8 @@ const LoginPage = () => {
                             <hr className='w-auto min-w-[100px] border' />
                         </div>
 
-                        <div className="grid w-full max-w-sm items-center gap-3">
+                        <div className='w-full h-auto flex flex-col'>
+                          <div className="grid w-full max-w-sm items-center gap-3">
                             <Label htmlFor="email">Email</Label>
                             <Input type="email" id="email" placeholder="example@gmail.com" />
                         </div>
@@ -41,6 +42,7 @@ const LoginPage = () => {
                             <Input type="password" id="password" placeholder="********" />
                         </div>
 
+                        </div>
                         <div className="flex items-center min-w-sm gap-3">
                             <Checkbox id="save" />
                             <Label htmlFor="save">Remember me</Label>
