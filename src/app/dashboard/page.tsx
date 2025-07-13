baa5c891ba935036100bb059f29fbe525d40430b
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-13 15:10:59 +0500

    strat creating dashnboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button';
 import Loading from '@/components/Loading';
 import { useRouter } from 'next/navigation';
 import Logo from '@/components/Logo';
+import Link from 'next/link';
 
 const Dashboard = () => {
     const { data: session, status } = useSession();
@@ -22,11 +23,21 @@ const Dashboard = () => {
         <div className='w-full h-auto flex'>
             <div className="side-menu relative max-w-xs h-screen">
                 <div className='fixed top-0 left-0 h-screen w-2xs bg-primary/5 text-primary/50'>
-                 <div className='w-full h-full flex justify-betweenan flex-col gap-2'>
-                    <div className='w-auto h-auto'>
-                        <Logo/>
+                    <div className='w-full h-full flex justify-betweenan flex-col gap-2'>
+                        <div className='w-auto h-auto'>
+                            <Logo />
+                        </div>
+                        <nav className='flex flex-col w-full h-auto py-4 gap-1'>
+                            <ul className='border-b border-primary-5 flex flex-col h-auto gap-2 w-full'>
+                                <li className='py-1 w-full h-auto hover:border-l active:border-l border-primary '>
+                                    <Link href='/' className='w-full h-auto hover:text-primary focus:text-primary'> Dashboard</Link>
+                                </li>
+                            </ul>
+                            <div className='h-auto w-full flex-col flex gap-2'>
+
+                            </div>
+                        </nav>
                     </div>
-                 </div>
                 </div>
             </div>
             <div className='w-full h-auto p-3'></div>
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Logo = () => {
     return (
-        <Image src='/Logo/logo.png' alt='Grow Easy Logo' width={150} height={150} />
+        <Image src='/Logo/logo.png' alt='Grow Easy Logo' width={130} height={130} />
     )
 }
 
