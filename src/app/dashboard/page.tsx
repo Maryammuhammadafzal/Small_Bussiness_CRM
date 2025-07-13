0ebf15d31c559e8ee71678f8e6d7debe0bdbb166
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-13 19:42:31 +0500

    creating dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,11 +6,12 @@ import Loading from '@/components/Loading';
 import { useRouter } from 'next/navigation';
 import Logo from '@/components/Logo';
 import Link from 'next/link';
-import { Bell, Box, ChartNoAxesColumn, ChartNoAxesColumnIncreasing, Clipboard, CloudDownload, Headphones, LayoutGrid, Search, Settings, Store, Users } from 'lucide-react';
+import { Bell, Box, ChartNoAxesColumn, ChartNoAxesColumnIncreasing, Clipboard, CloudDownload, EllipsisVertical, Headphones, LayoutGrid, Search, Settings, Store, TrendingUp, Users } from 'lucide-react';
 import Image from 'next/image';
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuList, NavigationMenuTrigger } from '@/components/ui/navigation-menu';
 import SideBar from '@/components/SideBar';
 import { Input } from '@/components/ui/input';
+import { Card, CardAction, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 const Dashboard = () => {
     const { data: session, status } = useSession();
@@ -36,12 +37,29 @@ const Dashboard = () => {
                             <h1 className='text-primary font-semibold text-3xl'>Hello, {session?.user?.name}👋</h1>
                             <p className='text-primary/50 px-1 text-xs'>You current dashboard for today</p>
                         </div>
-                        <div className='flex h-auto w-auto p-2 gap-2  relative'>
+                        <div className='flex h-auto w-auto p-2 gap-2 relative'>
                             <Input type='search' id="search" name='search' placeholder="Search" className='min-w-[230px] pl-8' />
-                            <Search className='absolute w-4 h-4 top-4.5 left-4 text-lg text-primary/50'/>
+                            <Search className='absolute w-4 h-4 top-4.5 left-4 text-lg text-primary/50' />
                             <Button className='bg-primary text-primary-foreground'><CloudDownload /> Export</Button>
                         </div>
                     </header>
+
+                    <div className='flex w-full flex-col gap-3 p-2'>
+                        <div className='w-full h-auto grid grid-cols-3 gap-4 justify-center items-center'>
+                            <Card>
+                                <CardHeader>
+                                    
+                                    <CardAction><EllipsisVertical size={20} className='text-primary/50' /></CardAction>
+                                </CardHeader>
+                                <CardContent className='flex flex-col gap-4'>
+                                    <div>
+                                        <CardTitle className='text-4xl font-medium gap-0 flex items-end'>$12,321 <span className='text-sm text-primary/50'>.32</span></CardTitle>
+                                    </div>
+                                    <p className='flex gap-1 font-mono text-sm'><span className=' flex gap-1'><TrendingUp size={20} /> 12%</span>from last week</p>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </div>
                 </div>
             </div>
             {/* <Button onClick={() => signOut()} className='bg-primary min-w-sm text-primary-foreground'>Log Out</Button> */}
