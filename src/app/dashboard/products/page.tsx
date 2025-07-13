9850fcfecf9e73963982df63d7b809263bd42d81
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-13 21:55:59 +0500

    Product Table

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,15 +1,18 @@
 import SideBar from '@/components/SideBar'
 import { Button } from '@/components/ui/button'
-import { Plus } from 'lucide-react'
+import { EllipsisVertical, Plus, Search } from 'lucide-react'
 import React from 'react'
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Card, CardAction, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import ProductTable from '@/components/ProductTable'
 
 const ProductPage = () => {
     return (
@@ -39,7 +42,31 @@ const ProductPage = () => {
                         </div>
                     </header>
 
-
+                    <Card>
+                        <CardHeader className='flex flex-col gap-1 w-full h-auto'>
+                            <div className='flex justify-between items-center w-full h-auto' >
+                                <CardTitle className='text-xl'>Products</CardTitle>
+                                <EllipsisVertical size={20} className='text-primary/50' />
+                            </div>
+                            {/* <div className='w-full h-auto flex justify-between relative items-center'>
+                                <Input type='search' id="search" name='search' placeholder="Search" className='w-xs pl-8' />
+                                <Search className='absolute w-4 h-4 top-2 left-2 text-lg text-primary/50' />
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger>Filter</DropdownMenuTrigger>
+                                    <DropdownMenuContent>
+                                        <DropdownMenuLabel>Product</DropdownMenuLabel>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuItem>All Product</DropdownMenuItem>
+                                        <DropdownMenuItem>Top Selling</DropdownMenuItem>
+                                        <DropdownMenuItem>Least Selling</DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                            </div> */}
+                        </CardHeader>
+                        <CardContent className='w-full'>
+                            <ProductTable/>
+                        </CardContent>
+                    </Card>
                 </div>
             </div>
             {/* <Button onClick={() => signOut()} className='bg-primary min-w-sm text-primary-foreground'>Log Out</Button> */}
