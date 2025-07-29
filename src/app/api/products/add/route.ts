12dee56bd0e083e3877c18e5f3a6acc517cdc68f
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-29 16:08:14 +0500

    adding multer

--- src/app/api/products/add/route.ts
+++ src/app/api/products/add/route.ts
@@ -1,27 +1,36 @@
 import { connectToDB } from "@/lib/db";
+import uploadMiddleware from "@/lib/upload";
 import Product from "@/models/Product";
+import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    await connectToDB(); 
+    await connectToDB();
+
+    await uploadMiddleware("product_images")(req as NextRequest);
 
     const body = await req.json();
-console.log(body);
+    console.log(body);
 
     const newProduct = await Product.create(body);
 
-    return NextResponse.json({
-      message: "Product added successfully!",
-      data: newProduct,
-    }, { status: 201 });
-
+    return NextResponse.json(
+      {
+        message: "Product added successfully!",
+        data: newProduct,
+      },
+      { status: 201 }
+    );
   } catch (error: any) {
     console.log("POST /api/products error:", error);
 
-    return NextResponse.json({
-      message: "Failed to add product",
-      error: error.message || "Something went wrong",
-    }, { status: 500 });
+    return NextResponse.json(
+      {
+        message: "Failed to add product",
+        error: error.message || "Something went wrong",
+      },
+      { status: 500 }
+    );
   }
 }
--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -1,6 +1,45 @@
+import multer from "multer";
 import { connectToDB } from "./db";
+import { NextApiRequest, NextApiResponse } from "next";
 
 connectToDB();
 
-const storage = 
-const upload =
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    console.log(req);
+    console.log(file);
+    console.log(cb);
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  },
+});
+
+const uploadMiddleware = (fieldName: string) => {
+  return async (
+    req: NextRequest,
+    res: NextResponse,
+    next: () => void
+  ) => {
+    try {
+      upload.array(fieldName)(req, res, async (err) => {
+        if (err) {
+          return res.status(400).json({ err: err.message });
+        }
+        if (!req.file) {
+          return res.status(400).json({ err: "Images are required" });
+        }
+        return req.file;
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ error: "Failed to upload images" });
+    }
+  };
+};
+
+export default uploadMiddleware;
