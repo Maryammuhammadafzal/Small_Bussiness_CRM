1a27193d7e30195ebabc8ce74940eacb9fd38690
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-30 12:27:24 +0500

    adding multer

--- src/app/api/products/add/route.ts
+++ src/app/api/products/add/route.ts
@@ -1,36 +1,53 @@
 import { connectToDB } from "@/lib/db";
-import uploadMiddleware from "@/lib/upload";
 import Product from "@/models/Product";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
+import bodyParser from "body-parser";
+import { uploadMiddleware } from "@/lib/upload";
 
-export async function POST(req: NextRequest) {
-  try {
-    await connectToDB();
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
-    await uploadMiddleware("product_images")(req as NextRequest);
-
-    const body = await req.json();
-    console.log(body);
-
-    const newProduct = await Product.create(body);
-
-    return NextResponse.json(
-      {
-        message: "Product added successfully!",
-        data: newProduct,
-      },
-      { status: 201 }
-    );
-  } catch (error: any) {
-    console.log("POST /api/products error:", error);
-
-    return NextResponse.json(
-      {
-        message: "Failed to add product",
-        error: error.message || "Something went wrong",
-      },
-      { status: 500 }
-    );
+export const handler = async (req: any, res: any) => {
+  // Check the Api Method
+  if (req.method !== "POST") {
+    return res.status(400).json({ message: "Only Post Method is allowed" });
   }
-}
+
+  // Connect to DB
+  await connectToDB();
+
+  uploadMiddleware("product_images")(req, res, async (err: any) => {
+    if (err)
+      return res
+        .status(400)
+        .json({ message: "File upload error", error: err.message });
+    try {
+      const body = await req.body();
+      console.log(body);
+
+      const newProduct = await Product.create(body);
+
+      return NextResponse.json(
+        {
+          message: "Product added successfully!",
+          data: newProduct,
+        },
+        { status: 201 }
+      );
+    } catch (error: any) {
+      console.log("POST /api/products error:", error);
+
+      return NextResponse.json(
+        {
+          message: "Failed to add product",
+          error: error.message || "Something went wrong",
+        },
+        { status: 500 }
+      );
+    }
+  });
+};
--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -1,45 +1,62 @@
 import multer from "multer";
 import { connectToDB } from "./db";
 import { NextApiRequest, NextApiResponse } from "next";
-
+import path from "path";
 connectToDB();
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const dir = './uploads/';
-        mkdirp(dir, err => cb(err, dir));
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
-    }
+  destination: "./public/uploads",
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, `${Date.now}-${file.fieldname}${ext}`);
+  },
 });
 
-const upload = multer({
-    storage: storage
-});
-
-
-const uploadMiddleware = (fieldName: string) => {
-  return async (
-    req: NextRequest,
-    res: NextResponse,
-    next: () => void
-  ) => {
-    try {
-      upload.array(fieldName)(req, res, async (err) => {
-        if (err) {
-          return res.status(400).json({ err: err.message });
-        }
-        if (!req.file) {
-          return res.status(400).json({ err: "Images are required" });
-        }
-        return req.file;
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(400).json({ error: "Failed to upload images" });
-    }
+export const uploadMiddleware = (fieldname: string) => {
+  const upload = multer({ storage });
+  return (req: any, res: any, next: any) => {
+    upload.array(fieldname)(req, res, (err) => {
+      if (err) return res.status(400).json({ err: err.message });
+      next();
+    });
   };
 };
 
-export default uploadMiddleware;
+// const runMiddleWare = (
+//   req: NextApiRequest,
+//   res: NextApiResponse,
+//   fn: Function
+// ) => {
+//   return new Promise((resolve, reject) => {
+//     fn(req, res, (result: any) => {
+//       if (result instanceof Error) {
+//         return reject(result);
+//       }
+//       return resolve(result);
+//     });
+//   });
+// };
+
+// Handle Images Upload
+// const uploadMiddleware = (feildName: string) => {
+//   const middleware = upload.array(feildName);
+
+//   return async (req: NextApiRequest, res: NextApiResponse) {
+//     try {
+//       await runMiddleWare(req , res , middleware);
+
+//       if(!req?.files || (Array.isArray(req?.files) && req?.files.length === 0)) {
+// return res.status(400).json({err: "No Image Uploaded"});
+//       }
+
+//       const uploadedImages = req?.files;
+//       return res.status(200).json({success: true , files : uploadedImages});
+
+//     } catch (error: any) {
+//       console.log(error);
+//       return res.status(500).json({ error: error.message || "Upload failed" });
+//     }
+//   }
+// };
+
+// export default uploadMiddleware;
